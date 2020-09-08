3d31bf848c7ff8b6629e81be8b99fb5b79a8429f
marcio-haytek <marcio.cruz@haytek.com.br>
2020-09-08 14:23:21 -0300

    ajustes 2

--- movie_service/app/api/service.py
+++ movie_service/app/api/service.py
@@ -3,9 +3,11 @@
 import os
 import httpx
 
-CAST_SERVICE_HOST_URL = 'http://localhost:8002/api/v1/casts/'
+CAST_SERVICE_HOST_URL = 'http://192.168.40.116:8080/api/v1/casts/'
 url = os.environ.get('CAST_SERVICE_HOST_URL') or CAST_SERVICE_HOST_URL
 
 def is_cast_present(cast_id: int):
+    print(cast_id)
+    print(url)
     r = httpx.get(f'{url}{cast_id}')
     return True if r.status_code == 200 else False
